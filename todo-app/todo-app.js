a8619cd0b66c13ddab056bd1dc310154ecdabbb0
iagonzal97 <iagonzal97@gmail.com>
2021-12-19 14:20:17 -0400

    completed the todo filter challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,28 +15,37 @@ const todos = [{ //array of objects
     completed: true
 }]
 
-//You have 2 todos left (p element)
-//Add a p for each todo above (use text value)
+//1. Setup a div contain for todos
+//2. Setup filters (searchText) and wire up a new filter input to change it
+//3. Create a renderTodos function to render and rerender the latest filtered data
 
-const incompleteTodos = todos.filter(function(todo){
-    return !todo.completed
-})
-
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
-
-const b = document.querySelector('body')
-let uncompletedCounter = 0
-for (let i = 0; i < todos.length; i++) {
-    const todo = todos[i];
-    if (!todo.completed){
-        uncompletedCounter++
-    }
-    const newParagraph = document.createElement('p')
-    newParagraph.textContent = todo.text
-    b.appendChild(newParagraph)
+const filters = {
+    searchText: ''
 }
+
+const renderTodos = function(todos, filters){
+    const filteredTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    const incompleteTodos = filteredTodos.filter(function(todo){
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary) 
+
+    filteredTodos.forEach(function(todo){
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
 
 //Listen for new todo creation
 document.querySelector('#add-todo').addEventListener('click', function(e){
@@ -46,9 +55,20 @@ document.querySelector('#add-todo').addEventListener('click', function(e){
 document.querySelector('#new-todo').addEventListener('input', function(e){
     console.log(e.target.value)
 })
-// const uncompletedCount = document.createElement('p')
-// uncompletedCount.textContent = `You have ${uncompletedCounter} todos left`
-// b.appendChild(uncompletedCount)
 
-// console.log(uncompletedCounter)
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
 
+// const b = document.querySelector('body')
+// let uncompletedCounter = 0
+// for (let i = 0; i < todos.length; i++) {
+//     const todo = todos[i];
+//     if (!todo.completed){
+//         uncompletedCounter++
+//     }
+//     const newParagraph = document.createElement('p')
+//     newParagraph.textContent = todo.text
+//     b.appendChild(newParagraph)
+// }
