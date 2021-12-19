24f9c437f1e8fba003693c0e3f376d62885866a8
iagonzal97 <iagonzal97@gmail.com>
2021-12-18 20:41:38 -0400

    finished the expense tracker

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,5 +1,6 @@
 const account = {
     name: 'Andrew Mead',
+    income: [],
     expenses: [],
     addExpense: function (description, amount) {
         let toAdd = {
@@ -8,14 +9,29 @@ const account = {
         }
         this.expenses.push(toAdd)
     },
+    addIncome: function (description, amount) {
+        let toAdd = {
+            description: description,
+            amount: amount
+        }
+        this.income.push(toAdd)
+    },
     getAccountSummary: function () {
         let name = this.name
         let expenses = this.expenses
-        let total = 0
-        for (let i = 0; i < expenses.length; i++) {
-            total += expenses[i].amount
+        let income = this.income
+        let balance = 0
+        let totalIncome = 0
+        let totalExpenses = 0
+        for (let i = 0; i < income.length; i++) {
+            totalIncome += income[i].amount
         }
-        return `${name} has $${total} in expenses.`
+        for (let i = 0; i < expenses.length; i++) {
+            totalExpenses += expenses[i].amount
+        }
+        balance = totalIncome - totalExpenses
+
+        return `${name} has a balance of $${balance}. $${totalIncome} in income.  $${totalExpenses} in expenses.`
     }
 }
 
@@ -39,8 +55,16 @@ const account = {
 //     return `${account.name} has $${total} in expenses.`
 // }
 
+//1. add income array to account
+//2. addIncome method -> description, amount
+//3. Tweak getAccountSummary
 
-account.addExpense('Rent', 950)
-account.addExpense('Coffee', 2)
+//Andrew Mead has a balance of $10. $100 in income. $90 in expenses.
+
+// account.addExpense('Rent', 950)
+// account.addExpense('Coffee', 2)
+// account.addIncome('Job', 1000)
+account.addIncome('example', 100)
+account.addExpense('example', 90)
 //console.log(account.expenses)
 console.log(account.getAccountSummary())
